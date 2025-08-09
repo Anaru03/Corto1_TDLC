bdce026a5cecf3ef5679dc26616cdee918892420
Anaru03 <dele22428@uvg.edu.gt>
2025-08-08 22:40:14 -0600

    Construcción de fragmentos de autómata

--- thompson.py
+++ thompson.py
@@ -15,3 +15,33 @@ def add_transition(s, sym, t):
     transitions[s].setdefault(sym, [])
     if t not in transitions[s][sym]:
         transitions[s][sym].append(t)
+
+def symbol(sym):
+    s = new_state()
+    a = new_state()
+    add_transition(s, sym, a)
+    return (s, [a])
+
+def union(frags):
+    ns = new_state()
+    na = new_state()
+    for f in frags:
+        add_transition(ns, 'ε', f[0])
+        for ac in f[1]:
+            add_transition(ac, 'ε', na)
+    return (ns, [na])
+
+def concat(f1, f2):
+    for ac in f1[1]:
+        add_transition(ac, 'ε', f2[0])
+    return (f1[0], f2[1])
+
+def star(f):
+    ns = new_state(); na = new_state()
+    add_transition(ns, 'ε', f[0]); add_transition(ns, 'ε', na)
+    for ac in f[1]:
+        add_transition(ac, 'ε', f[0]); add_transition(ac, 'ε', na)
+    return (ns, [na])
+
+def plus(f):
+    return concat(f, star(f))
