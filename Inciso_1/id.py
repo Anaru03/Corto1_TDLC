05b875027414aad6b3d1964612505685551c6fb6
Anaru03 <dele22428@uvg.edu.gt>
2025-08-08 23:11:20 -0600

    Incisos restantes

---
+++ Inciso_1/id.py
@@ -0,0 +1,106 @@
+# Ejercicio 1
+##  Inciso 1 — AFN para letter
+
+from pprint import pprint
+import networkx as nx
+import matplotlib.pyplot as plt
+
+transitions = {} 
+state_counter = 0
+
+def new_state():
+    global state_counter, transitions
+    s = state_counter
+    state_counter += 1
+    transitions[s] = {}
+    return s
+
+def add_transition(s, sym, t):
+    transitions.setdefault(s, {})
+    transitions[s].setdefault(sym, [])
+    if t not in transitions[s][sym]:
+        transitions[s][sym].append(t)
+
+def symbol(sym):
+    s = new_state()
+    a = new_state()
+    add_transition(s, sym, a)
+    return (s, [a])
+
+def union(frags):
+    ns = new_state()
+    na = new_state()
+    for f in frags:
+        add_transition(ns, 'ε', f[0])
+        for ac in f[1]:
+            add_transition(ac, 'ε', na)
+    return (ns, [na])
+
+def concat(f1, f2):
+    for ac in f1[1]:
+        add_transition(ac, 'ε', f2[0])
+    return (f1[0], f2[1])
+
+def star(f):
+    ns = new_state(); na = new_state()
+    add_transition(ns, 'ε', f[0]); add_transition(ns, 'ε', na)
+    for ac in f[1]:
+        add_transition(ac, 'ε', f[0]); add_transition(ac, 'ε', na)
+    return (ns, [na])
+
+def plus(f):
+    return concat(f, star(f))
+
+letters = [symbol(x) for x in ['A','B','a','b']]
+letter_frag = union(letters)
+
+print("letter start:", letter_frag[0], "accepts:", letter_frag[1])
+print("Transiciones:")
+pprint(transitions)
+
+start = letter_frag[0]
+accepts = set(letter_frag[1])
+
+# Crear grafo dirigido
+G = nx.DiGraph()
+for s, mapping in transitions.items():
+    for sym, dests in mapping.items():
+        for d in dests:
+            G.add_edge(s, d, label=sym)
+
+# Dibujar grafo
+pos = nx.spring_layout(G)  # Layout automático
+nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=700)
+nx.draw_networkx_labels(G, pos)
+nx.draw_networkx_edges(G, pos, arrowstyle='->', arrowsize=15)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, 'label'))
+
+nx.draw_networkx_nodes(G, pos, nodelist=[start], node_color='green', node_size=700)
+nx.draw_networkx_nodes(G, pos, nodelist=list(accepts), node_color='orange', node_size=700)
+
+plt.title("AFN de 'letter'")
+plt.axis('off')
+plt.show()
+
+
+## Inciso 2 — digit y digits
+
+digits_syms = [symbol(str(i)) for i in range(10)]
+digit_frag = union(digits_syms)
+digits_frag = plus(digit_frag)  # digit+
+
+print("digit start:", digit_frag[0], "accepts:", digit_frag[1])
+print("digits (digit+) start:", digits_frag[0], "accepts:", digits_frag[1])
+
+
+## Inciso 3- id letter
+letter_or_digit = union([letter_frag, digit_frag])
+letter_or_digit_star = star(letter_or_digit)
+id_frag = concat(letter_frag, letter_or_digit_star)
+print("id start:", id_frag[0], "accepts:", id_frag[1])
+
+
+
+
+
+
